d979b61b8ae2bf3ea40b6be76f0f7f26dab9147f
thebikesh13-dot <thebikesh13@gmail.com>
2026-02-21 23:16:59 +0545

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Love Dashboard</title>
+<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<h2>You & Your Partner 💖</h2>
+<div class="actions">
+<button onclick="send('💋 Kiss')">💋</button>
+<button onclick="send('💭 Imagining You')">💭</button>
+<button onclick="send('💞 Flying Kiss')">💞</button>
+</div>
+<div id="toast"></div>
+<script>const PARTNER_ID=1;</script>
+<script src="assets/js/app.js"></script>
+</body>
+</html>
